125b29e3d5d9e2796bbf0a0b88fd2411e3a2853f
VulcanWM <logmedha09@gmail.com>
2023-04-25 21:00:35 +0100

    trying to connect to mongodb

--- add_contributor.js
+++ add_contributor.js
@@ -1,6 +1,24 @@
-const { readFileSync, writeFileSync } = require("fs");
-const path = "CONTRIBUTORS.json";
-const jsonString = readFileSync(path);
-const contributors = JSON.parse(jsonString);
-contributors['Hello'] = "hi"
-writeFileSync(path, JSON.stringify(contributors, null, 2), "utf8");
+const uri = process.env.MONGODB_URI
+// const uri = "mongodb+srv://Hello:There@contributors.gg8ibc3.mongodb.net/?retryWrites=true&w=majority"
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("Logs").collection("Users").insertOne({"Username": "Hello"})
+    console.log("added user")
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
