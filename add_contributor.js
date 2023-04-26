b22db137e851b4b6735a36c04e811b987a7f845e
VulcanWM <logmedha09@gmail.com>
2023-04-26 18:03:33 +0100

    adds tokens to user every time they push

--- add_contributor.js
+++ add_contributor.js
@@ -15,8 +15,25 @@ const client = new MongoClient(uri,  {
 async function run() {
   try {
     await client.connect();
-    await client.db("Logs").collection("Users").insertOne({"Username": username})
-    console.log("added user")
+    const users = await client.db("Logs").collection("Users").find({"Username": username}).toArray();
+    if (users.length == 0){
+        await client.db("Logs").collection("Users").insertOne(
+            {
+                "Username": username,
+                "Tokens": 1,
+            }
+        )
+        console.log("added user")
+    } else {
+        await client.db("Logs").collection("Users").deleteOne({"Username": username})
+        await client.db("Logs").collection("Users").insertOne(
+            {
+                "Username": username,
+                "Tokens": users[0]['Tokens'] + 1,
+            }
+        )
+        console.log("updated user")
+    }
   } finally {
     await client.close();
   }
