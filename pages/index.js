66472d9efc1e047b57c7789dcd42526c50d065c9
VulcanWM <logmedha09@gmail.com>
2023-04-26 21:04:11 +0100

    added next app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>GitHub Actions MongoDB</title>
+        <meta name="description" content="github actions mongodb test" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello</h1>
+    </>
+  )
+}
