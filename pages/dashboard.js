4b0116bef5edcc6afd04bdf6ea4b6339a1cc355a
VulcanWM <logmedha09@gmail.com>
2023-04-27 21:38:33 +0100

    shows tokens

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
-// import { authOptions } from 'pages/api/auth/[...nextauth]'
+import clientPromise from "../lib/mongodb";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next"
 import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home( { username, stats } ) {
+export default function Home( { stats } ) {
+  stats = JSON.parse(stats)
   return (
     <div>
       <Head>
@@ -15,7 +15,8 @@ export default function Home( { username, stats } ) {
       </Head>
     <>
         <div>
-        <h4>Signed in as {username}</h4>
+        <h4>Signed in as {stats['Username']}</h4>
+        <p>Tokens: {stats['Tokens']}</p>
         <button onClick={() => signOut()}>Sign out</button>
         </div>
     </>
@@ -25,7 +26,6 @@ export default function Home( { username, stats } ) {
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions)
-
   if (!session) {
     return {
       redirect: {
@@ -34,10 +34,25 @@ export async function getServerSideProps(context) {
       },
     }
   }
+  const userid = session.user.image.replace("https://avatars.githubusercontent.com/u/", "").replace("?v=4", "")
+  const res = await fetch(
+    `https://api.github.com/user/${userid}`
+  );
+  const data = await res.json();
+  const username = data['login']
+  const client = await clientPromise;
+  const db = client.db("Logs");
+  const users = await db.collection("Users").find({"Username": username}).toArray();
+  var stats = {}
+  if (users.length == 0){
+    stats = {"Username": username, "Tokens": 0}
+  } else {
+    stats = users[0]
+  }
+  stats = JSON.stringify(stats)
   return {
     props: {
-      username: session.user.name,
-      stats: {}
+      stats
     },
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-// import { authOptions } from 'pages/api/auth/[...nextauth]'
 import { authOptions } from "./api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next"
 import { useSession, signIn, signOut } from "next-auth/react";
